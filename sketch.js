e28b5d161bfca8d2b8d4cd5d8ad4968e99b64e89
yarogniew <1@yarogniew.net>
2018-03-16 22:09:32 +0100

    wersja końcowa

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 var song;
-var slider;
+var sliderVolume;
+var sliderRate;
+var sliderPan;
 
 
 function setup() {
   createCanvas(200, 200);
   song = loadSound("szumy.mp3", loaded);
-  slider = createSlider(0, 1, 0.5, 0.01)
+  sliderVolume = createSlider(0, 1, 0.5, 0.01);
+  sliderRate = createSlider(0, 3, 1, 0.01);
+  sliderPan = createSlider(-1, 1, 0, 0.01);
 }
 
 function loaded(){
@@ -15,5 +19,8 @@ function loaded(){
 function draw(){
   background(random(0,255));
 //  console.log(slider.value());
-    song.setVolume(slider.value());
+    song.setVolume(sliderVolume.value());
+    song.rate(sliderRate.value());
+    song.pan(sliderPan.value());
+
 }
